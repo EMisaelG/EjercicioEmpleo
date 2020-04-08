5833f736d889ebd3f85f9bcd5927d8b5074f0901
Misael <mizahel696@gmail.com>
2020-04-07 19:20:13 -0500

    Añadiendo pug

--- src/index.js
+++ src/index.js
@@ -4,9 +4,14 @@ const app = express();
 //Servicios
 app.use(express.json());
 app.use(express.urlencoded({extends: false}));
+app.set('view engine', 'pug');
 
 //Rutas
 app.use(require('./routes/index'));
+
+app.get('/', (req, res) =>{
+    res.render('index', {});
+});
 
 app.listen(3000);
 console.log('Online');
